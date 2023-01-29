d2ac05e0e662191521303e69822a0d62d19126c9
Daniella Omokore <daniellaomokore@gmail.com>
2023-01-29 10:59:47 +0000

    commit

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
-USER = "root"
-PASSWORD = "****!" # THIS IS YOUR MYSQLWORKBENCH PASSWORD, MAKE SURE YOU FILL IN
-HOST = "localhost"
+
+
+import os
+
+
+# Mysqlworkbench
+
+DATABASENAME = os.environ.get('DATABASENAME')
+USER = os.environ.get('USER')
+PASSWORD = os.environ.get('DATABASEPASSWORD') # THIS IS YOUR MYSQLWORKBENCH PASSWORD
+HOST = os.environ.get('HOST')
 
